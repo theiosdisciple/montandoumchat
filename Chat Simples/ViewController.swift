924dc231516f851ab94e3b9b8941a84b03868608
Gabriel Bezerra Valério <gbrvalerio@gmail.com>
2017-09-09 11:44:13 -0300

    Initial Commit

---
+++ Chat Simples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chat Simples
+//
+//  Created by Gabriel Bezerra Valério on 09/09/17.
+//  Copyright © 2017 The iOS Disciple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
